d8bee347a5e2a5fbd75840c6b0777adb7f88227c
Jake Harris <milieu@javakat.io>
2015-02-02 23:18:42 -0600

    Create dotfiles installer. Great idea by @smockle!

---
+++ link-dotfiles.sh
@@ -0,0 +1,23 @@
+
+# idea from @smockle (http://github.com/smockle/dotfiles)
+#!/usr/bin/env sh
+
+platform=''
+case "$OSTYPE" in
+  solaris*) platform='solaris' ;;
+  darwin*)  platform='osx' ;;
+  linux*)   platform='linux' ;;
+  bsd*)     platform='bsd' ;;
+  *)        platform='windows' ;;
+esac
+
+ln -s ~/Projects/dotfiles/.bash_profile ~/.bash_profile
+
+rm ~/.bashrc
+ln -s ~/Projects/dotfiles/.bashrc ~/.bashrc
+
+rm ~/.gitconfig
+ln -s ~/Projects/dotfiles/.gitconfig ~/.gitconfig
+if [[ $platform == 'windows' ]]; then
+  git config --global credential.helper "!'C:\\Users\\unknownhybrid\\AppData\\Roaming\\GitCredStore\\git-credential-winstore.exe'"
+fi
