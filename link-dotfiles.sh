b85ba7de08f2f1cf3927660f175448a7f211191c
Jake Harris <jake.harris@synapsewireless.com>
2019-08-09 10:53:05 -0500

    Update installation script for dotfiles

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -1,6 +1,8 @@
 
-# idea from @smockle (http://github.com/smockle/dotfiles)
 #!/bin/bash
+# idea from @smockle (http://github.com/smockle/dotfiles)
+
+set -e
 
 platform=''
 case "$OSTYPE" in
@@ -11,13 +13,23 @@ case "$OSTYPE" in
   *)        platform='windows' ;;
 esac
 
-ln -s ~/Projects/dotfiles/.bash_profile ~/.bash_profile
-
 rm ~/.bashrc
 ln -s ~/Projects/dotfiles/.bashrc ~/.bashrc
 
-rm ~/.gitconfig
-ln -s ~/Projects/dotfiles/.gitconfig ~/.gitconfig
-if [[ $platform == 'windows' ]]; then
-  git config --global credential.helper "!'C:\\Users\\unknownhybrid\\AppData\\Roaming\\GitCredStore\\git-credential-winstore.exe'"
+if [[ $platform == 'osx' ]]; then
+  pushd ~/Library/Application\ Support/Code/User
+
+  if [ -f 'settings.json' ]; then
+    rm settings.json
+  fi
+  if [ -f 'keybindings.json' ]; then
+    rm keybindings.json
+  fi
+
+  ln -s ~/Projects/dotfiles/vs-code/settings.json    ./settings.json
+  ln -s ~/Projects/dotfiles/vs-code/keybindings.json ./keybindings.json
+  popd
 fi
+
+
+
